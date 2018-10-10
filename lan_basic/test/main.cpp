1c52212d4c9c66c6e14033bc4c6d7ef02789e776
chanchann <www.769440972@qq.com>
2018-10-11 00:16:23 +0800

    update

---
+++ lan_basic/test/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    vector<vector<int>> a={
+            {1,1,1,1},
+            {1,1,1,1},
+            {1,1,1,1},
+            {1,1,1,1},
+    };
+    cout<<a<<endl;
+    return 0;
+}
